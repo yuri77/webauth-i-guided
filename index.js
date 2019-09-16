057d73a00bc6c2eed2a08c72de134ba2f9092ab2
yurika sakae <yurika.sun@gmail.com>
2019-09-16 10:21:51 -0700

    credintials set up

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.get("/", (req, res) => {
 
 server.post("/api/register", (req, res) => {
   let { username, password } = req.body;
-  const hash = bcrypt.hashSync(password, 8);
+  const hash = bcrypt.hashSync(password);
 
   Users.add({ username, password: hash })
     .then(saved => {
@@ -35,7 +35,7 @@ server.post("/api/login", (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
